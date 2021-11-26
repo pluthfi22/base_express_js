f29eba1138f1a8551a1feed3adb0ff6f9128ce48
PeterPorker <94734288+pluthfi22@users.noreply.github.com>
2021-11-27 01:17:15 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'your_telegram_token';
+const token = '2012325488:AAEhUAGCInp0a1YFK7kSIVykFlY9_kihDbA';
 const bot = new TelegramBot(token, {polling: true});
 
 
